5274d697c7e6a0e5c4ca03d4493dd1aa881015a8
TAHAR Noureddine <engr.tahar.noureddine@gmail.com>
2024-12-10 16:11:56 +0100

    Initial Commit

---
+++ Football Chants/Football_ChantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Football_ChantsApp.swift
+//  Football Chants
+//
+//  Created by TAHAR Noureddine on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Football_ChantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
