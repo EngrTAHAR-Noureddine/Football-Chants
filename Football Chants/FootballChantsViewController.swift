78869665d7d3b972013b671b888e5bdf66b7a2dd
TAHAR Noureddine <engr.tahar.noureddine@gmail.com>
2024-12-20 16:41:55 +0100

    Add Resources

---
+++ Football Chants/FootballChantsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FootballChantsViewController.swift
+//  Football Chants
+//
+//  Created by TAHAR Noureddine on 20/12/2024.
+//
+import UIKit
+
+class FootballChantsViewController: UIViewController {
+    override func viewDidLoad() {
+        print("FootballChantsViewController : viewDidLoad")
+        super.viewDidLoad()
+        
+        self.title = "Hello World"
+    
+        self.view.backgroundColor = .systemRed
+        
+    }
+}
